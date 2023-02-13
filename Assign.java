b24973ffd63b1f67e91a8f5086085e2281256499
pranitlama <pranitlama211@gmail.com>
2023-02-13 17:28:28 +0545

    complete

---
+++ Assign.java
@@ -0,0 +1,15 @@
+public class Assign{
+    public static void main(String[] args){
+        System.out.println("Name of the organization:Kamalpokhari Resturant & Bar");
+        System.out.println("\n");
+        System.out.println("Address:");
+         System.out.println("----State: KTM");
+         System.out.println("----Zip Code: 44600");
+         System.out.println("----Country: Nepal");
+        System.out.println("\n");
+        System.out.println("Contact Infomation:");
+         System.out.println("----Mobile Number: 986532409");
+         System.out.println("----Email: kamalpokhari.rnb@gmail.com");
+
+    }
+}
